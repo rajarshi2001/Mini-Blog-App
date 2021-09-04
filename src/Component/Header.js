499ab5c67db20f527b90b9b9e79a3b3997629e1a
Rajarshi <rajarshisaha66@gmail.com>
2021-09-04 13:55:52 +0530

    anothers commit

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
         <>
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
                 <div className="container-fluid">
-                    <Link to="/" className="navbar-brand text-white" >Mini bolgs</Link>
+                    <Link to="/" className="navbar-brand text-white" >Mini blogs</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
